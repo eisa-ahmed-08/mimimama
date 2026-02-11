c59c2eefbdda07fa1ca88d377dac6b16aa5041f2
eisa-ahmed-08 <eisa.ahmed@iisgalvanimi.edu.it>
2026-02-11 13:54:00 +0000

    punto 1

---
+++ script.js
@@ -0,0 +1,54 @@
+var myGameArea = {
+    canvas: document.createElement("canvas"),
+    start: function () {
+        this.canvas.width = 480;
+        this.canvas.height = 270;
+        this.context = this.canvas.getContext("2d");
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+    },
+    clear: function () {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    },
+    draw: function (component) {
+        this.context.fillStyle = component.color;
+        this.context.fillRect(component.x, component.y, component.width, component.height);
+    }
+};
+
+var redSquare = {
+    width: 20,
+    height: 20,
+    x: 10,
+    y: 120,
+    color: "red"
+};
+
+function startGame() {
+    myGameArea.start();
+    myGameArea.draw(redSquare);
+}
+
+function updateGameArea() {
+    myGameArea.clear();
+    myGameArea.draw(redSquare);
+}
+
+function moveup() {
+    redSquare.y -= 30;
+    updateGameArea();
+}
+
+function movedown() {
+    redSquare.y += 30;
+    updateGameArea();
+}
+
+function moveleft() {
+    redSquare.x -= 30;
+    updateGameArea();
+}
+
+function moveright() {
+    redSquare.x += 30;
+    updateGameArea();
+}
